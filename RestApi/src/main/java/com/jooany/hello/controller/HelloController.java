7b58288328c48c8aa332e9d1a196efe103339425
jooany <83267254+jooany@users.noreply.github.com>
2021-12-01 13:44:29 +0900

    first commit

---
+++ RestApi/src/main/java/com/jooany/hello/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.jooany.hello.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins="http://localhost:8001")
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+	@ResponseBody
+	public Map<String, String> apiJson() { 
+		final Map<String, String> map = new HashMap<>(); 
+		map.put("message", "Welcome");
+		return map; 	
+	}
+}
---
+++ RestApi/src/test/java/com/jooany/hello/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jooany.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
