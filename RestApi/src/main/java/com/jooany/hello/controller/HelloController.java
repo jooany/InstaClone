9374bddf19c2a80e8fb76bcfceb06b24ff23bb84
jooany <83267254+jooany@users.noreply.github.com>
2021-12-02 21:17:00 +0900

    cors 해결 쫌

--- RestApi/src/main/java/com/jooany/hello/controller/HelloController.java
+++ RestApi/src/main/java/com/jooany/hello/controller/HelloController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="http://18.217.255.224:8001")
 @RestController
 public class HelloController {
 	@GetMapping("/hello")
 	@ResponseBody
 	public Map<String, String> apiJson() { 
 		final Map<String, String> map = new HashMap<>(); 
-		map.put("message", "Welcome");
+		map.put("message", "Welcome!");
 		return map; 	
 	}
 }
