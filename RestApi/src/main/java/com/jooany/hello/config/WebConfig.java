44f395e3d7669298ca68aa5fed20049a15b2b31c
jooany <83267254+jooany@users.noreply.github.com>
2021-12-02 21:38:38 +0900

    config로 변경

---
+++ RestApi/src/main/java/com/jooany/hello/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.jooany.hello.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://18.217.255.224:8001");
+  }
+}
